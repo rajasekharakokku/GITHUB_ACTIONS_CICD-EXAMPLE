d3856a3047bcd277f1ec4009e304c70cfc54dda9
Rajasekhara Naidu Kokku <138103223+rajasekharkokku@users.noreply.github.com>
2024-03-04 22:27:44 +0800

    first commit

---
+++ src/test/java/com/spring/github/actions/github_actions/GithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.github.actions.github_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
