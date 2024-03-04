4e53639de5950423b63b4be86d393009417eb29c
Rajasekhara Naidu Kokku <138103223+rajasekharkokku@users.noreply.github.com>
2024-03-04 22:31:39 +0800

    first commit

--- src/main/java/com/spring/github/actions/github_actions/GithubActionsApplication.java
+++ src/main/java/com/spring/github/actions/github_actions/GithubActionsApplication.java
@@ -2,12 +2,29 @@ package com.spring.github.actions.github_actions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubActionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithubActionsApplication.class, args);
-	}
+    @GetMapping("/welcome")
+    public String welcome()
+    {
+        return "Welcome to the GIT HUB Actions CI CD";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsApplication.class, args);
+    }
+
+//    echo "# GITHUB_ACTIONS_CICD-EXAMPLE" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M Master
+//    git remote add origin https://github.com/rajasekharakokku/GITHUB_ACTIONS_CICD-EXAMPLE.git
+//    git push -u origin Master
+
 
 }
--- src/test/java/com/spring/github/actions/github_actions/GithubActionsApplicationTests.java
+++ src/test/java/com/spring/github/actions/github_actions/GithubActionsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GithubActionsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
